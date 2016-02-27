4a5230adb6bffc204678420f216c075e5f27fb53
Luke Gessler <ldg3fa@virginia.edu>
2016-02-27 13:27:26 -0500

    json updates

--- app/server/codeSearchMethod.js
+++ app/server/codeSearchMethod.js
@@ -20,6 +20,13 @@ var lineToResultObj = function(line) {
   return newObj;
 };
 
+// Client can emit two messages: one with a single attribute, "kill", set to true, and one with
+// a single attribute, "term", representing the user's query. Depending on which we see,
+// either kill the user's process (because she has navigated away from the results page),
+// or spawn a new one.
+//
+// We use Streamy's s.id attribute to uniquely map from client to child process. This is to
+// ensure that with more than one user, we will only ever kill that user's process.
 var procs = {};
 Streamy.on('search', function(d, s) {
   var o;
@@ -35,7 +42,7 @@ Streamy.on('search', function(d, s) {
     }
   }
   else {
-    csearch = spawn('/home/luke/.go/bin/csearch', ['-n', d.term]);
+    csearch = spawn(Meteor.settings.CSEARCH_PATH, ['-n', d.term]);
     procs[s.id] = csearch;
 
     csearch.stdout.on('data', function (data) {
@@ -44,5 +51,17 @@ Streamy.on('search', function(d, s) {
         Streamy.emit('search', o, s);
       }
     });
+  }
+});
+
+/* INDEX UPDATE */
+// This code calls cindex every hour to check for changes to local files.
+var callCindex = function() {
+  spawn(Meteor.settings.CINDEX_PATH); // no args--we already know _what_ to index
+};
+
+var cron = new Meteor.Cron({
+  events: {
+    "0 * * * *": callCindex
   }
 });
