7d0ab8888e1f20e9f227ea6af7ca93293c83929e
Luke Gessler <ldg3fa@virginia.edu>
2016-02-28 15:26:05 -0500

    small updates

--- app/client/templates/search/searchResults.js
+++ app/client/templates/search/searchResults.js
@@ -1,11 +1,61 @@
 
+// Though we put this here because it's logically associated with this template,
+// note that this is active _globally_ on all templates
+Streamy.on('search', function(d) {
+  if (Router.current().route.getName() === "searchResults") {
+
+    if (d.type === 'DATA') {
+      console.log("received data", d);
+      var data = d.data;
+      data.text = data.text.replace('\n', ' ');
+
+      Blaze.renderWithData(
+          Template.searchResultItem,
+          data,
+          $("#searchResults")[0],  // parent node
+          $("#scrollDiv")[0]       // node to insert before
+      );
+    } else {
+      console.log("received non-data message", d);
+      Session.set('moreResults', false);
+    }
+  } else {
+    throw "Got an object " + d.toString() + ", but we aren't on searchResults!";
+  }
+});
+
 Template.searchResults.created = function () {
-  Streamy.emit('search', {term: Session.get('term')});
+  Session.set('moreResults', true);
+
+  Streamy.emit('search', {
+    type: 'TERM',
+    term: Session.get('term')
+  });
+};
+
+Template.searchResults.destroyed = function () {
+  Streamy.emit('search', {
+    type: 'KILL'
+  });
+};
+
+Template.searchResults.rendered = function () {
+  this.$("#searchResults").bind('scroll', function() {
+    console.log($(this).scrollTop(), $(this).innerHeight(), $(this)[0].scrollHeight);
+    if($(this).scrollTop() + $(this).innerHeight()>=$(this)[0].scrollHeight) {
+      if (Session.get('moreResults')) {
+        Streamy.emit('search', {type: 'INCR', amt: 30});
+      }
+    }
+  });
 };
 
 Template.searchResults.helpers({
   term: function() {
     return Router.current().params.term;
+  },
+  moreResults: function() {
+    return Session.get('moreResults');
   }
 });
 
--- app/server/codeSearchMethod.js
+++ app/server/codeSearchMethod.js
@@ -20,43 +20,91 @@ var lineToResultObj = function(line) {
   return newObj;
 };
 
-// Client can emit two messages: one with a single attribute, "kill", set to true, and one with
-// a single attribute, "term", representing the user's query. Depending on which we see,
-// either kill the user's process (because she has navigated away from the results page),
-// or spawn a new one.
-//
+
 // We use Streamy's s.id attribute to uniquely map from client to child process. This is to
 // ensure that with more than one user, we will only ever kill that user's process.
 var procs = {};
+var limits = {};
+var numsSent = {};
+var lineBuffers = {};
 Streamy.on('search', function(d, s) {
   var o;
-  console.log("Server hears an emission on 'search'", d);
-
+  var lineBuffer = lineBuffers[s.id];
   var csearch = procs[s.id];
 
-  // if it already exists, kill it
-  if (d.kill) {
-    if (csearch) {
-      csearch.kill('SIGKILL');
-      delete procs[s.id];
-    }
-  }
-  else {
-    csearch = spawn(Meteor.settings.CSEARCH_PATH, ['-n', d.term]);
-    procs[s.id] = csearch;
+  console.log("Server hears an emission on 'search'", d, "from client", s.id);
 
-    csearch.stdout.on('data', function (data) {
-      o = lineToResultObj(data.toString());
-      if (o) {
-        Streamy.emit('search', o, s);
+  switch (d.type) {
+    case 'KILL':
+      if (csearch) {
+        csearch.kill('SIGKILL');
+        delete procs[s.id];
       }
-    });
+      break;
+
+    case 'INCR':
+      if (csearch && numsSent[s.id] >= limits[s.id] - d.amt) {
+        limits[s.id] += d.amt;
+        csearch.kill('SIGCONT');
+      }
+      else {
+        throw new Meteor.Error(500, "Tried to INCR a csearch that didn't exist!");
+      }
+      break;
+
+    // we have a new search--spawn a new process for it
+    case 'TERM':
+      csearch = spawn(Meteor.settings.CSEARCH_PATH, ['-n', d.term]);
+      procs[s.id] = csearch;
+      numsSent[s.id] = 0;
+      limits[s.id] = 20;
+      lineBuffer = [];
+      lineBuffers[s.id] = lineBuffer;
+
+      csearch.stdout.on('data', function (data) {
+        //console.log(data.toString(), numsSent[s.id], "of", limits[s.id]);
+
+        // NOTE: this could give us more than one line at a time, so we must split on \n
+        var lines = data.toString().split('\n');
+        lines = lines.slice(0, lines.length-1); //get rid of empty line at the end
+        lines.forEach(function(line) {
+          lineBuffer.push(line);
+        });
+
+        while (lineBuffer[numsSent[s.id]] && numsSent[s.id] < limits[s.id]) {
+          o = lineToResultObj(lineBuffer[numsSent[s.id]]);
+          if (o) {
+            Streamy.emit('search', {
+              type: 'DATA',
+              data: o
+            }, s);
+            numsSent[s.id]++;
+          } else {
+            lineBuffer = lineBuffer.splice(numSent, 1);
+          }
+        }
+
+        if (numsSent[s.id] >= limits[s.id]) {
+          csearch.kill('SIGSTOP');
+        }
+
+      });
+
+      // let client know when we're done
+      csearch.on('close', function() {
+        Streamy.emit('search', {type: 'DONE'}, s);
+      });
+      break;
+
+    default:
+      throw new Meteor.Error(500, "Malformed Streamy emission received on server--how did that happen?");
   }
 });
 
 /* INDEX UPDATE */
 // This code calls cindex every hour to check for changes to local files.
 var callCindex = function() {
+  console.log("Running cindex cron job");
   spawn(Meteor.settings.CINDEX_PATH); // no args--we already know _what_ to index
 };
 
