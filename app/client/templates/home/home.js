095d760f8c7bae1eecfdd9b7f90776b487d30124
Luke Gessler <ldg3fa@virginia.edu>
2016-04-17 14:49:58 -0400

    minor

--- app/client/templates/home/home.js
+++ app/client/templates/home/home.js
@@ -1,17 +1,25 @@
 Template.home.rendered = function() {
-
+  Session.setDefault('advancedMode', false);
 };
+
 Template.home.events({
   "submit form": function(e) {
     e.preventDefault();
     var term = $(e.target).find('[name=term]').val();
     Session.set('prevTerm', term);
     Router.go('searchResults', {'term': term} );
+  },
+  "click #modebutton" : function(e) {
+    e.preventDefault();
+    Session.set('advancedMode', !Session.get('advancedMode'));
   }
 });
 
 Template.home.helpers({
   prevTerm: function() {
     return Session.get('prevTerm');
+  },
+  advanced: function() {
+    return Session.get('advancedMode');
   }
 });
